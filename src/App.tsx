cbe3db77ab346f8bc61de86e353ab1d469611183
salasj1 <assalas.19@est.ucab.edu.ve>
2025-04-15 12:49:13 -0400

    solo modificacion de una ruta

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,6 @@ import ControlAutorizacion from './routes/ControlAutorizacion.tsx';
 import ChangePasswordVerify from './routes/ChangePasswordVerify.tsx';
 import ConstaciaDeTrabajo from './routes/ConstaciaDeTrabajo.tsx';
 import RetornoVacaciones from './routes/RetornoVacaciones.tsx';
-import Expendiente from './routes/Expendiente.tsx';
 import AdminDashboard from './routes/AdminDashboard.tsx';
  function App() {
   return (
@@ -48,7 +47,6 @@ import AdminDashboard from './routes/AdminDashboard.tsx';
             <Route key="ProcesarPermisos" path="ProcesarPermisos" element={<ProcesarPermisos/>} />,
             <Route key="DirectorioEmpleados" path="DirectorioEmpleados" element={<DirectorioEmpleados/>} />,
             <Route key="ControlSupervision" path='ControlSupervision' element={<ControlAutorizacion />} />,
-            <Route key='ayuda' path='ayuda' element={<Expendiente />} />,
             <Route key='Admin' path='Admin' element={<AdminDashboard />} />
           ]}
         ></Route>
